8154e7bcd98e4ad4bfc36df94fcf2af9c4ba454b
abhbaner <abhbaner@redhat.com>
2022-10-09 12:40:56 +0530

    Added node setup for ansible playbook runs - ssh keys, host entries

--- cloudk8sSetup_aws.sh
+++ cloudk8sSetup_aws.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
-echo "Initializing Terraform"
+: <<'END_COMMENT'
+This script does the below 
+- Run Terraform automation
+- Extract IP information from Terraform output
+- Add host entries to all the nodes ( Ansible Master, k8s Master,  k8s worker)
+- Add public ssh key of Ansible master in k8s nodes
+- Install Ansible on the Ansible master node
+- Execute playbooks to install k8s cluster (k8s-pkg.yml, k8s-master.yml,k8s-worker.yml)
+  and install k8s networking
+END_COMMENT
+
+# Running Terraform Automation
+echo "****** Initializing Terraform ******"
 terraform init
-echo "Running Terraform automation"
+echo "****** Running Terraform automation ******"
 terraform apply --auto-approve
-echo "Storing hosts information"
+echo "****** Storing hosts information ******"
 terraform output > output_cloud.txt
 cat output_cloud.txt
+
 ### Extracting public and private IPs of all the 3VMs and assigning them to vars ###
 ANSIBLE_PUBLIC="$(grep -E 'ansibleMaster_public_ips' output_cloud.txt)"
 ANSIBLE_PRIVATE="$(grep -E 'ansibleMaster_private_ips' output_cloud.txt)"
@@ -13,9 +26,9 @@ K8SMASTER_PUBLIC="$(grep -E 'k8smaster_public_ip' output_cloud.txt)"
 K8SMASTER_PRIVATE="$(grep -E 'k8smaster_private_ip' output_cloud.txt)"
 K8SWORKER_PUBLIC="$(grep -E 'k8sworker_public_ips' output_cloud.txt)"
 K8SMASTER_PRIVATE="$(grep -E 'k8smaster_private_ip' output_cloud.txt)"
-splitfunc(){
+getIP(){    #Gets the IP value  
     OIFS=$IFS
-    IFS=$'='
+    IFS=$'=' #split string 
     echo $1
     CTR=0
     for x in $1
@@ -26,27 +39,62 @@ splitfunc(){
         fi
         CTR=$((CTR+1))
     done  
-    retval=$x  
+    x="${x//\"}" #remote quotation and remove the leading space
+    ip="${x:1}"
+    retval=$ip
+    
 }
 
-splitfunc "$ANSIBLE_PUBLIC"
+getIP "$ANSIBLE_PUBLIC"
 ansible_pub_ip=$retval
-splitfunc "$ANSIBLE_PRIVATE"
+getIP "$ANSIBLE_PRIVATE"
 ansible_priv_ip=$retval
-splitfunc "$K8SMASTER_PUBLIC"
+getIP "$K8SMASTER_PUBLIC"
 k8smaster_pub_ip=$retval
-splitfunc "$K8SMASTER__PRIVATE"
+getIP "$K8SMASTER__PRIVATE"
 k8smaster_priv_ip=$retval
-splitfunc "$K8SWORKER_PUBLIC"
+getIP "$K8SWORKER_PUBLIC"
 k8sworker_pub_ip=$retval
-splitfunc "$K8SWORKER_PRIVATE"
+getIP "$K8SWORKER_PRIVATE"
 k8sworker_priv_ip=$retval
 
-echo $ansible_pub_ip
-echo $ansible_priv_ip
-echo $k8smaster_pub_ip
-echo $k8smaster_priv_ip
-echo $k8sworker_pub_ip
-echo $k8sworker_priv_ip
+#Adding entries to /etc/hosts of all nodes
+USERNAME='ubuntu@'
+HOSTENTRY1="echo $ansible_priv_ip ansible@example.com  ansible | sudo tee -a /etc/hosts"
+HOSTENTRY2="echo $k8smaster_priv_ip k8smaster@example.com  k8smaster | sudo tee -a /etc/hosts"
+HOSTENTRY3="echo $k8sworker_priv_ip ansible@example.com  k8sworker| sudo tee -a /etc/hosts"
+ssh -o StrictHostKeyChecking=no -i pptestkey.pem ${USERNAME}${ansible_pub_ip} "${HOSTENTRY2}"
+for HOSTIP in $ansible_pub_ip $k8smaster_pub_ip $k8sworker_pub_ip ; do
+    for CMD in $HOSTENTRY1 $HOSTENTRY2 $HOSTENTRY3 ; do
+        echo "****** Adding host entries in $HOSTIP ******"
+        ssh -o StrictHostKeyChecking=no -i pptestkey.pem ${USERNAME}${HOSTIP} "${CMD}"
+    done    
+done
 
+#generating ssh key on Ansible node and adding them to k8s node's authorized key
+echo "****** Copying ec2 key from execution server to ansible node ******"
+scp -i pptestkey.pem pptestkey.pem ${USERNAME}${ansible_pub_ip}:/tmp
+echo "****** Generating SSH key on the Ansible master and copying it to k8s node ******"
+ssh -o StrictHostKeyChecking=no -i pptestkey.pem ${USERNAME}${ansible_pub_ip} << END
+    echo -e "\n\n\n" | ssh-keygen -t rsa
+    cd ~/.ssh
+    cp /tmp/pptestkey.pem .
+    echo "scp to k8s master>>>>>>>>>>>>>>"
+    scp -o StrictHostKeyChecking=no -i pptestkey.pem id_rsa.pub ubuntu@k8smaster:/tmp
+    echo "scp to k8s worker>>>>>>>>>>>>>>"
+    scp -o StrictHostKeyChecking=no -i pptestkey.pem id_rsa.pub ubuntu@k8sworker:/tmp
+    pwd
+END
 
+echo "****** Appending authorizedkey file on k8s node with Ansible node's public key ******"
+ssh -o StrictHostKeyChecking=no -i pptestkey.pem ${USERNAME}${k8smaster_pub_ip} << END
+    cd /tmp
+    cat id_rsa.pub >> ~/.ssh/authorized_keys
+END
+echo "Ansible public key successfully added to k8s master"
+ 
+ssh -o StrictHostKeyChecking=no -i pptestkey.pem ${USERNAME}${k8sworker_pub_ip} << END
+    cd /tmp
+    cat id_rsa.pub >> ~/.ssh/authorized_keys
+END
+echo "Ansible public key successfully added to k8s worker"
